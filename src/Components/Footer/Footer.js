0b5fa1184af4086270286c8abae74e3760659c74
pranavz <mailz4pranav@gmail.com>
2022-04-20 03:18:57 +0530

    2nd

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import '../../App.css';
+import { BsPinterest } from 'react-icons/bs';
+import { MdFacebook } from 'react-icons/md';
+
+import {
+  AiFillTwitterCircle,
+  AiFillYoutube,
+  AiFillGooglePlusCircle,
+  AiOutlineDribbble,
+  AiFillGithub,
+  AiOutlineCodepen,
+} from 'react-icons/ai';
 
 function Footer() {
   return (
     <div className="Footer">
-      <h1>icons</h1>
+
+      <MdFacebook className="icon" />
+      <AiFillTwitterCircle className="icon" />
+      <AiFillYoutube className="icon" />
+      <AiFillGooglePlusCircle className="icon" />
+      <AiOutlineDribbble className="icon" />
+      <BsPinterest className="icon" />
+      <AiFillGithub className="icon" />
+      <AiOutlineCodepen className="icon" />
 
     </div>
   );
